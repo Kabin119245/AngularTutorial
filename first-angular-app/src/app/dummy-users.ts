608d6ed93e51e5586c8b209125f3e98c3793c972
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-09 17:12:30 +0545

    property binding, string interpolation, event listen

---
+++ first-angular-app/src/app/dummy-users.ts
@@ -0,0 +1,22 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Kabin Giri',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Biraj Subedi',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Kanchan Dhamala',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Anshu Baskota',
+    avatar: 'user-4.jpg',
+  },
+];
