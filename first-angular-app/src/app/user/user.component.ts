56259bf12fc948dbd3e863b185f238950ddf5148
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-09 18:41:37 +0545

    implemented signals

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,14 +12,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //option-1
+  //selectedUser = DUMMY_USERS[randomIndex];
+  //option-2 using signal
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(
+    () => '../../assets/users/' + this.selectedUser().avatar
+  );
 
-  get imagePath() {
-    return '../../assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return '../../assets/users/' + this.selectedUser.avatar;
+  // }
+  //we dont use getter in the signal
 
   onSelectUser() {
-    console.log('Clicked!');
-    
+    //console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+
+    //using signal
+
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
