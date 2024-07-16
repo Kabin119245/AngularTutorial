8395fc54cd2c69d4ca33462d0332ccbdcc700226
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-16 23:07:58 +0545

    outputting task data

--- 01-easy-task/src/app/dummy-tasks.ts
+++ 01-easy-task/src/app/dummy-tasks.ts
@@ -8,7 +8,7 @@ export const dummyTasks = [
     dueDate: '2025-12-31',
   },
   {
-    id: 't1',
+    id: 't2',
     userId: 'u1',
     title: 'Master Angular',
     summary:
@@ -16,14 +16,14 @@ export const dummyTasks = [
     dueDate: '2025-12-31',
   },
   {
-    id: 't2',
+    id: 't3',
     userId: 'u2',
     title: 'Build first prototype',
     summary: 'Build a first prototype of the online shop website',
     dueDate: '2024-05-31',
   },
   {
-    id: 't3',
+    id: 't4',
     userId: 'u3',
     title: 'Prepare issue template',
     summary:
@@ -31,7 +31,7 @@ export const dummyTasks = [
     dueDate: '2024-06-15',
   },
   {
-    id: 't3',
+    id: 't5',
     userId: 'u4',
     title: 'Prepare issue template',
     summary:
--- 01-easy-task/src/app/tasks/tasks.component.ts
+++ 01-easy-task/src/app/tasks/tasks.component.ts
@@ -13,47 +13,7 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't3',
-      userId: 'u2',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't4',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-    {
-      id: 't5',
-      userId: 'u4',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  tasks = dummyTasks;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
