efccc32e4cb98a2a0665218de4a1476406beac6c
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-16 19:29:31 +0545

    user and role controllers added

--- 02-1-angular-springboot-backend/src/main/java/com/crud/springbootbackend/controllers/UserController.java
+++ 02-1-angular-springboot-backend/src/main/java/com/crud/springbootbackend/controllers/UserController.java
@@ -2,12 +2,15 @@ package com.crud.springbootbackend.controllers;
 
 
 
+import com.crud.springbootbackend.entities.User;
 import com.crud.springbootbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -18,8 +21,39 @@ public class UserController {
     UserService userService;
 
     //create new user
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
 
-    @PostMapping
+    //Get user by id
+
+    @GetMapping(path = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity <User> getUserById(@PathVariable String userId) {
+        User foundUser = userService.getUserById(userId);
+        return new ResponseEntity<User>(foundUser, HttpStatus.FOUND);
+    }
+
+    //get all users
+    @GetMapping(produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> usersList = userService.getAllUsers();
+        return new ResponseEntity<List<User>>(usersList,HttpStatus.OK);
+    }
+
+    //Update user
+    @PutMapping(consumes =MediaType.APPLICATION_JSON_VALUE,produces =MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        User UserUpdated = userService.updateUser(user);
+        return new ResponseEntity<User>(UserUpdated,HttpStatus.ACCEPTED);
+    }
+    //Delete user
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 
 
