8d878c2a223224d4caa17ec6d8fc9a289185245a
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-20 22:24:19 +0545

    conditionally rendering component

--- 01-easy-task/src/app/tasks/tasks.component.ts
+++ 01-easy-task/src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from './../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -13,17 +14,19 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
 
+  isAddingTask = false;
+
   tasks = dummyTasks;
 
-  onAddClickStart() {}
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => {
-      task.id !== id;
-    });
+    this.tasks = this.tasks.filter((task) => task.id != id);
   }
 }
