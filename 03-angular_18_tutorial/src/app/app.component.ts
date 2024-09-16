eb97c05c6b45bc7d84095d8504fe17ebd8fe7818
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-16 19:44:04 +0545

    working with components

--- 03-angular_18_tutorial/src/app/app.component.ts
+++ 03-angular_18_tutorial/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = '01-angular_18_tutorial';
