f6291fce915c3c67c1aa71b135c07f548af363f5
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-17 21:42:12 +0545

    *ngIf and *ngFor implemented

--- 03-angular_18_tutorial/src/app/components/directive/structural-directive/structural-directive.component.ts
+++ 03-angular_18_tutorial/src/app/components/directive/structural-directive/structural-directive.component.ts
@@ -14,6 +14,22 @@ export class StructuralDirectiveComponent {
 
   isDiv2enabled: boolean = true;
 
+  cityArray: string[] = [
+    'Kathmandu',
+    'Pokhara',
+    'Lumbini',
+    'Dolakha',
+    'Dhankuta',
+  ];
+
+  studentList: any[] = [
+    { studId: 1, name: 'Kabin', city: 'Kathmandu', isActive: true },
+    { studId: 2, name: 'Kabina', city: 'Kathmandu', isActive: false },
+    { studId: 3, name: 'Kanchan', city: 'Dhankuta', isActive: true },
+    { studId: 4, name: 'Kaluram', city: 'Doha', isActive: false },
+    { studId: 5, name: 'Laxmi', city: 'Dhankuta', isActive: false },
+  ];
+
   num1: string = '';
   num2: string = '';
 
