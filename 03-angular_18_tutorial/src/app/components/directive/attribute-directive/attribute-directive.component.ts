6984b587746b6a89bf3b75f8f306e90c33c5e3b8
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-20 21:17:57 +0545

    ngClass and ngStyle Completed

--- 03-angular_18_tutorial/src/app/components/directive/attribute-directive/attribute-directive.component.ts
+++ 03-angular_18_tutorial/src/app/components/directive/attribute-directive/attribute-directive.component.ts
@@ -1,14 +1,61 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-attribute-directive',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './attribute-directive.component.html',
   styleUrl: './attribute-directive.component.css',
 })
 export class AttributeDirectiveComponent {
+  num1: string = '';
+  num2: string = '';
+
+  studentList: any[] = [
+    {
+      studId: 1,
+      gender: 'Male',
+      name: 'Kabin',
+      totalMarks: 64,
+      city: 'Kathmandu',
+      isActive: true,
+    },
+    {
+      studId: 2,
+      gender: 'feMale',
+      name: 'Kabina',
+      totalMarks: 55,
+      city: 'Kathmandu',
+      isActive: false,
+    },
+    {
+      studId: 3,
+      gender: 'feMale',
+      name: 'Kanchan',
+      totalMarks: 82,
+      city: 'Dhankuta',
+      isActive: true,
+    },
+    {
+      studId: 4,
+      gender: 'Male',
+      name: 'Kaluram',
+      totalMarks: 89,
+      city: 'Doha',
+      isActive: false,
+    },
+    {
+      studId: 5,
+      gender: 'feMale',
+      totalMarks: 45,
+      name: 'Laxmi',
+      city: 'Dhankuta',
+      isActive: false,
+    },
+  ];
+
   div1BgColor: string = 'bg-danger';
 
   isDiv2Active: boolean = false;
