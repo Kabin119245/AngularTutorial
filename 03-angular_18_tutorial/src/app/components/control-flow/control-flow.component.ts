cbf39056a4cf3171b79ba89b19fafce2ce9fb583
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-24 19:08:27 +0545

    @if,  @else if, @else implemented

--- 03-angular_18_tutorial/src/app/components/control-flow/control-flow.component.ts
+++ 03-angular_18_tutorial/src/app/components/control-flow/control-flow.component.ts
@@ -1,12 +1,32 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css',
 })
-export class ControlFlowComponent {}
+export class ControlFlowComponent {
+  div1Visible: boolean = true;
+
+  isWarningDivVisible: boolean = true;
+
+  num1: string = '';
+  num2: string = '';
+
+  selectedStatus: string = '';
+
+  hideDiv1() {
+    this.div1Visible = false;
+  }
+
+  showDiv1() {
+    this.div1Visible = true;
+  }
+
+  toggleDiv2() {
+    this.isWarningDivVisible = !this.isWarningDivVisible;
+  }
+}
