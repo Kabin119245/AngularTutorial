d2992907737de83b05d0ae67652c8f72d1439987
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-30 23:02:48 +0545

    POST method implemented

--- 03-angular_18_tutorial-part2-fromAPI/src/app/app.routes.ts
+++ 03-angular_18_tutorial-part2-fromAPI/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { GetApiComponent } from './components/api-integration/get-api/get-api.component';
+import { PostApiComponent } from './components/api-integration/post-api/post-api.component';
 
 export const routes: Routes = [
   {
     path: 'get-api',
     component: GetApiComponent,
+  },
+
+  {
+    path: 'post-api',
+    component: PostApiComponent,
   },
 ];
---
+++ 03-angular_18_tutorial-part2-fromAPI/src/app/components/api-integration/post-api/post-api.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-api',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './post-api.component.html',
+  styleUrl: './post-api.component.css',
+})
+export class PostApiComponent {
+  departmentObj: any = {
+    departmentId: 0,
+    departmentName: '',
+    departmentLogo: '',
+  };
+
+  http = inject(HttpClient);
+
+  onSave() {
+    this.http
+      .post(
+        'https://projectapi.gerasim.in/api/Complaint/AddNewDepartment',
+        this.departmentObj
+      )
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert('Department created successfully');
+        } else {
+          alert(res.message);
+        }
+      });
+  }
+}
